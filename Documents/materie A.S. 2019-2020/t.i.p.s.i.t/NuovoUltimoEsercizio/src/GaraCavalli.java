d7b85d4bc305a73900241292732ac9f0334835c7
diego taioli <diegotaioli2002@gmail.com>
2020-06-01 23:29:20 +0200

    contenitore degli oggetti

--- Documents/materie A.S. 2019-2020/t.i.p.s.i.t/NuovoUltimoEsercizio/src/GaraCavalli.java
+++ Documents/materie A.S. 2019-2020/t.i.p.s.i.t/NuovoUltimoEsercizio/src/GaraCavalli.java
@@ -1,7 +1,11 @@
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.util.Scanner;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -12,31 +16,42 @@ import javax.swing.JLabel;
  *
  * @author Diego
  */
-public class GaraCavalli {
+public class GaraCavalli extends JFrame{
     private Cavallo_in_Corsa [] cav;
+    Animazione[] parte;
     Thread[] concorrenti;
     private int num_corsie = 2;
+    Pista campo;
     int posizione;
+    Graphics graf;
+    Image virtual;
     
     public GaraCavalli(int cor){
         this.num_corsie = cor;
+        setSize(1000, 845);
+        setLocation(10, 40);
         /*do {
             System.out.println("Inserisci il numero: ");
             Scanner input = new Scanner(System.in);
             num_corsie = input.nextInt();
         }while(num_corsie < 2|| num_corsie>10);*/
         cav = new Cavallo_in_Corsa[num_corsie];
+        parte = new Animazione[num_corsie];
         concorrenti = new Thread[num_corsie];
-        
+        campo = new Pista(num_corsie);
+        int start = 15;
         for(int i = 0; i<num_corsie; i++){
-            cav[i] = new Cavallo_in_Corsa(i+1, this);
+            parte[i] = new Animazione(start, i+1);
+            cav[i] = new Cavallo_in_Corsa(parte[i], i+1, this);
             concorrenti[i] = new Thread(cav[i]);
+            start = start + 100;        
         }
         posizione = 1;
         for(int i = 0; i<num_corsie; i++){
             concorrenti[i].start();
         }
-        
+        this.add(campo);
+        setVisible(true);
     }
     
     public int getCorsie(){
@@ -70,14 +85,34 @@ public class GaraCavalli {
         classificato.setBackground(Color.BLUE);
         classificato.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         classificato.getContentPane().setLayout(new GridLayout(getCorsie(),1));
-        int pos = 0;
-        for(int j = 1; j < this.getCorsie(); j++){
+        int pos = 0, i = 0;
+        for(int j = 0; j < this.getCorsie(); j++){
+            i++;
             pos = cav[j].posizione;
-            terminati[j] = new JLabel("il numero: "+j+" è arrivato: "+pos);
+            terminati[j] = new JLabel("il numero: "+i+" è arrivato: "+pos);
             terminati[j].setFont(new Font("TImes New Roman", Font.ITALIC,20));
             terminati[j].setForeground(Color.blue);
             classificato.getContentPane().add(terminati[j]);
         }
         classificato.setVisible(true);
+    }
+    
+    public void update(Graphics g){
+        paint(g);
+    }
+    
+    public void paint(Graphics g){
+        if(parte != null){
+            Graphics2D schermo = (Graphics2D)g;
+            virtual = createImage(1000, 845);
+            graf = virtual.getGraphics();
+            Dimension d = getSize();
+            campo.paint(graf);
+            for(int i = 0; i<this.num_corsie; i++){
+                parte[i].paint(graf);
+            }
+            schermo.drawImage(virtual, 0, 20, this);
+            graf.dispose();
+        }
     }
 }
