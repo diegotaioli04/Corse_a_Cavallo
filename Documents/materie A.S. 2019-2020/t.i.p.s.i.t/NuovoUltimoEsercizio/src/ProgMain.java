071c88d69ba0a7b12f2229a205d12b5a462140a2
diego taioli <diegotaioli2002@gmail.com>
2020-06-01 23:32:06 +0200

    classe main

---
+++ Documents/materie A.S. 2019-2020/t.i.p.s.i.t/NuovoUltimoEsercizio/src/ProgMain.java
@@ -0,0 +1,17 @@
+
+
+/**
+ *
+ * @author Diego
+ */
+public class ProgMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Interfaccia inter = new Interfaccia();
+    }
+    
+}
